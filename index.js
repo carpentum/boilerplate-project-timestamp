73f7b2d3038e9b65437fd6b31f0a67b1759c09f6
Ramón <ramon.mosquera@gmail.com>
2023-11-29 10:04:07 +0100

    First try

--- index.js
+++ index.js
@@ -25,7 +25,7 @@ app.get("/api/hello", function (req, res) {
 });
 
 app.get("/api/:date?", function (req, res) {
-  let input = request.params.date;
+  let input = req.params.date;
   let responseObject = {};
 
   if (input.includes("-")) {
